3fc878d979a2bba675ad3990dfa3f6b5b8088e75
vinaydawani <vdawani6@gmail.com>
2021-11-19 08:20:01 -0500

    command services. abstraction bb

---
+++ src/service/dashboard_service.py
@@ -0,0 +1,16 @@
+from store import euphoriaData
+
+
+def getData():
+    data = {}
+
+    data["WAGMI Price"] = euphoriaData.wagmi_price
+    data["Market Cap"] = euphoriaData.market_cap
+    data["TVL"] = euphoriaData.staking_TVL
+    data["APY"] = euphoriaData.staking_APY
+    data["Current Index"] = euphoriaData.currentIndex
+    data["Treasury balance"] = ""
+    data["Backing per $WAGMI"] = ""
+    data["Runway"] = ""
+
+    return data
---
+++ src/service/stake_service.py
@@ -0,0 +1,40 @@
+import web3
+import abi
+
+from helpers import web3Helper
+from store import euphoriaData, userData
+
+
+def get_basic_info():
+    data = {}
+
+    data["APY"] = euphoriaData.staking_APY
+    data["TVL"] = euphoriaData.staking_TVL
+    data["Current Index"] = euphoriaData.currentIndex
+
+    return data
+
+
+def get_user_data(addr):
+    data = {}
+
+    user_data = userData.get_user_balance(addr)
+
+    data["Your Balance"] = user_data["wagmi"]
+    data["Your Staked Balance"] = user_data["swagmi"]
+    data["Next Reward Amount"] = (euphoriaData.staking_rebase / 100) * user_data[
+        "swagmi"
+    ]
+    data["Next Reward Yield"] = euphoriaData.staking_rebase
+    data["ROI (5-Day Rate)"] = euphoriaData.five_day_rate
+
+    return data
+
+
+def getData(addr):
+    if addr:
+        d1 = get_basic_info()
+        d2 = get_user_data(addr)
+        return {**d1, **d2}
+    else:
+        return get_basic_info()
